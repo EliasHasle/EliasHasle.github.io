a8f0e4396970891037dd41a0b71b315d4b982804
EliasHasle <elias.hasle@gmail.com>
2017-07-29 18:27:53 +0200

    Added LazyBinaryTree
    Random.js is now a mess of old and new (experimental, not done).

--- Random.js
+++ Random.js
@@ -8,13 +8,22 @@ Todo:
 
 var Random = function() {
 	var state = 123456789;
+	var stateHI = 153453643; //experimental
 
-	var seed = function(s) {
+	var seed = function(s, hi) {
 		if (s === 0 || isNaN(s)) {
 			console.log("Invalid seed (" + s + "). Using time instead.");
 			s = Date.now();
 		}
 		state = s;
+		//experimental:
+		if (hi !== undefined) {
+			if (isNaN(hi)) {
+				console.log("Invalid hi seed (" + hi + "). Using value from time instead.");
+				hi = 153453643^Date.now();
+			}
+			stateHI = hi;
+		}
 	};
 
 	//Source: https://en.wikipedia.org/wiki/Xorshift
@@ -24,6 +33,39 @@ var Random = function() {
 		state ^= (state << 5);
 		return state;
 	};
+	
+	//Experimental:
+	var xorshift64 = function() {
+		//x ^= x >> 12 :
+		let hi = stateHI^(stateHI>>>12);
+		let lo = state^((state>>>12)|((stateHI&0xFFF)<<20));
+		stateHI = hi;
+		state = lo;
+		
+		//x ^= x << 25 :
+		hi = stateHI^((stateHI<<25)|(state>>>7));
+		lo = state^(state<<25);
+		stateHI = hi;
+		state = lo;
+		
+		//x ^= x >> 27 :
+		hi = stateHI^(stateHI>>>27);
+		lo = state^((state>>>27)|((stateHI&0x7FFFFFF)<<5));
+		stateHI = hi;
+		state = lo;
+	}
+	
+	//experimental
+	var rand53 = function() {
+		xorshift64();
+		return ((stateHI&0x1fffff)*0x100000000+state)/0x20000000000000;
+	}
+	
+	//experimental
+	var rInt32_from64 = function() {
+		xorshift64();
+		return state;
+	}
 
 	//TODO: Improve this to generate 53 bits instead of 32:
 	var rand = function() {
